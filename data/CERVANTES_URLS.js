50412c4c612c36c942eb3afc6ff4beb691385dcd
lingora-labs <adolfobalaguer7@gmail.com>
2026-03-15 12:53:49 +0100

    Restore data folder from official ZIP
    Restore data folder from official ZIP

---
+++ data/CERVANTES_URLS.js
@@ -0,0 +1,13 @@
+export const CERVANTES_URLS = [
+  'https://www.cervantes.es/sobre_instituto_cervantes/informacion.htm',
+  'https://examenes.cervantes.es/es/dele/que-es',
+  'https://examenes.cervantes.es/es/dele/niveles',
+  'https://examenes.cervantes.es/es/ccse/que-es',
+  'https://www.cervantes.es/lengua_y_ensenanza/aprender_espanol.htm'
+];
+
+export const LINGORA_URLS = [
+  'https://lingora.com/programas/espana',
+  'https://lingora.com/metodologia',
+  'https://lingora.com/faq'
+];
