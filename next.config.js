67e95762cb3e43252c0cbf5e5ef828510edba49b
lingora-labs <adolfobalaguer7@gmail.com>
2026-03-20 13:19:01 +0100

    chore(next): remove invalid config
    fix(build): align contracts, protocol and clean next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['openai', 'pdf-lib', '@aws-sdk/client-s3', '@aws-sdk/client-rekognition'],
-}
+const nextConfig = {}
 
 module.exports = nextConfig
